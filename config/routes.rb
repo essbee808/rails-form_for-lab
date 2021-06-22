cfda90c72dc06736b99be339c5598cbf81478386
essbee808 <sheena.myb@gmail.com>
2021-06-21 22:56:19 -0700

    generate school_classes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'school_classes/new'
+
+  get 'school_classes/show'
+
+  get 'school_classes/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
