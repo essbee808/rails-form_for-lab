a966536997b38c05461581f9e6a442e7dec48433
essbee808 <sheena.myb@gmail.com>
2021-06-21 23:42:59 -0700

    add patch routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   patch 'students/:id', to: 'students#update'
 
   resources :school_classes, only: [:new, :create, :show, :edit]
+  patch 'school_classes/:id', to: 'school_classes#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
