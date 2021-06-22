4a427f7bbade5e0185befcad39b565d70ffcda64
essbee808 <sheena.myb@gmail.com>
2021-06-21 23:14:37 -0700

    add form_for to student view

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  get 'students/new'
 
-  get 'students/show'
-
-  get 'students/edit'
-
-  get 'school_classes/new'
-
-  get 'school_classes/show'
-
-  get 'school_classes/edit'
+  resources :students, only: [:new, :create, :show, :edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
